dde6c9985d7f1e06657e20d5737bd58d7fbe68d5
nguetcheu <dnguetcheu@gmail.com>
2024-12-14 13:20:29 +0100

    first commit

---
+++ app/src/main/java/com/example/fragmentapp/FirstFragment.java
@@ -0,0 +1,58 @@
+package com.example.fragmentapp;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Toast.makeText(getActivity(), "On Attach() is called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getActivity(), "On Create() is called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(getActivity(), "On Resume() is called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        Button firstBtn = view.findViewById(R.id.btn_frag1);
+        TextView text = view.findViewById(R.id.text_frag1);
+
+        firstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Welcome to the first fragment", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
